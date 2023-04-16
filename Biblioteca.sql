6aac1bef234d8faa6031a8fcefd2ec23c82bb85e
Cristian Palta <cristianpalta@MacBook-Air-de-Cristian.local>
2023-04-16 12:45:42 -0500

    ON DELETE CASCADE actualizacion llaves foraneas

--- Biblioteca.sql
+++ Biblioteca.sql
@@ -10,14 +10,14 @@ CREATE TABLE libro (
   titulo varchar(20) DEFAULT NULL,
   editorial varchar(20) DEFAULT NULL,
   id_autor int(11) DEFAULT NULL,
-  FOREIGN KEY (id_autor) REFERENCES autor(id_autor)
+  FOREIGN KEY (id_autor) REFERENCES autor(id_autor) ON DELETE CASCADE
 );
 
 CREATE TABLE libro_prestamo (
   id_libro_prestamo int(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   id_prestamo int(11) DEFAULT NULL,
   id_libro int(11) DEFAULT NULL,
-  FOREIGN KEY (id_libro) REFERENCES libro (id_libro)
+  FOREIGN KEY (id_libro) REFERENCES libro (id_libro) ON DELETE CASCADE
 
 );
 
@@ -30,7 +30,7 @@ CREATE TABLE prestamo (
   id_prestamo int(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   diashabiles int(20) DEFAULT NULL,
   id_multa int(11) DEFAULT NULL,
-  FOREIGN KEY (id_multa) REFERENCES multa (id_multa)
+  FOREIGN KEY (id_multa) REFERENCES multa (id_multa) ON DELETE CASCADE
 );
 
 CREATE TABLE usuario (
@@ -39,7 +39,7 @@ CREATE TABLE usuario (
   edad int(3) DEFAULT NULL,
   numero_cont int(10) DEFAULT NULL,
   id_prestamo int(11) DEFAULT NULL,
-  FOREIGN KEY (id_prestamo) REFERENCES prestamo (id_prestamo)
+  FOREIGN KEY (id_prestamo) REFERENCES prestamo (id_prestamo) ON DELETE CASCADE
 
 )
 
